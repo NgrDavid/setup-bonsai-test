02047ac368e51832f3034efeaecb8f064a2bfcfd
David Maas <d.maas+git@neurogears.org>
2025-05-11 09:43:42 -0500

    Replaced packages with ones which definitely have to be injected

--- docs/export-images.ps1
+++ docs/export-images.ps1
@@ -1,4 +1,5 @@
 Push-Location $PSScriptRoot
+.\bonsai\modules\Export-Image.ps1 -workflowPath './workflows' -bootstrapperPath '../.bonsai/Bonsai.exe'
 foreach ($environment in (Get-ChildItem -Path 'examples/' -Filter '.bonsai' -Recurse -Directory))
 {
     .\bonsai\modules\Export-Image.ps1 -workflowPath ($environment.Parent.FullName) -bootstrapperPath (Join-Path $environment.FullName 'Bonsai.exe')
