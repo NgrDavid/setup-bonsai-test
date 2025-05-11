969a0ddfe780dd272739c4def03db16bcf6d8cdf
David Maas <d.maas+git@neurogears.org>
2025-05-11 14:11:02 -0500

    powershell more like power hell lmaooooo

--- docs/export-images.ps1
+++ docs/export-images.ps1
@@ -10,10 +10,10 @@ if (!$IsWindows) {
     $monoPrefix = 'mono '
 }
 
-.\bonsai\modules\Export-Image.ps1 -workflowPath './workflows' -bootstrapperPath $monoPrefix + '../.bonsai/Bonsai.exe'
+.\bonsai\modules\Export-Image.ps1 -workflowPath './workflows' -bootstrapperPath ($monoPrefix + '../.bonsai/Bonsai.exe')
 foreach ($environment in (Get-ChildItem -Path 'examples/' -Filter '.bonsai' -Recurse -Directory))
 {
-    .\bonsai\modules\Export-Image.ps1 -workflowPath ($environment.Parent.FullName) -bootstrapperPath $monoPrefix + (Join-Path $environment.FullName 'Bonsai.exe')
+    .\bonsai\modules\Export-Image.ps1 -workflowPath ($environment.Parent.FullName) -bootstrapperPath ($monoPrefix + (Join-Path $environment.FullName 'Bonsai.exe'))
 }
 
 Pop-Location
