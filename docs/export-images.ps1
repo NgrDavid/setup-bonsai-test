1898e0db95fb5b3b9a2cc1d22f84990acc43c7f6
David Maas <d.maas+git@neurogears.org>
2025-05-11 14:05:36 -0500

    distractions

--- docs/export-images.ps1
+++ docs/export-images.ps1
@@ -4,9 +4,16 @@ $ErrorActionPreference = 'Stop'
 $PSNativeCommandUseErrorActionPreference = $true
 
 Push-Location $PSScriptRoot
-.\bonsai\modules\Export-Image.ps1 -workflowPath './workflows' -bootstrapperPath '../.bonsai/Bonsai.exe'
+
+$monoPrefix = ''
+if (!$IsWindows) {
+    $monoPrefix = 'mono '
+}
+
+.\bonsai\modules\Export-Image.ps1 -workflowPath './workflows' -bootstrapperPath $monoPrefix + '../.bonsai/Bonsai.exe'
 foreach ($environment in (Get-ChildItem -Path 'examples/' -Filter '.bonsai' -Recurse -Directory))
 {
-    .\bonsai\modules\Export-Image.ps1 -workflowPath ($environment.Parent.FullName) -bootstrapperPath (Join-Path $environment.FullName 'Bonsai.exe')
+    .\bonsai\modules\Export-Image.ps1 -workflowPath ($environment.Parent.FullName) -bootstrapperPath $monoPrefix + (Join-Path $environment.FullName 'Bonsai.exe')
 }
+
 Pop-Location
