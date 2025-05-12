33238afaebe7c633cf0f6baaa673c536ded7cbb9
David Maas <d.maas+git@neurogears.org>
2025-05-11 19:37:11 -0500

    RIP Linux and macOS, Mono doesn't support my criminal activity.

--- docs/export-images.ps1
+++ docs/export-images.ps1
@@ -6,6 +6,7 @@ $PSNativeCommandUseErrorActionPreference = $true
 Push-Location $PSScriptRoot
 
 .\bonsai\modules\Export-Image.ps1 -workflowPath './workflows' -bootstrapperPath '../.bonsai/Bonsai.exe'
+
 foreach ($environment in (Get-ChildItem -Path 'examples/' -Filter '.bonsai' -Recurse -Directory))
 {
     .\bonsai\modules\Export-Image.ps1 -workflowPath ($environment.Parent.FullName) -bootstrapperPath (Join-Path $environment.FullName 'Bonsai.exe')
